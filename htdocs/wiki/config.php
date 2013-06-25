0c99f2933bc9d3018c2a199404b2d9f0a0de500c
Sploinga <github@sploing.fr>
2013-06-25 17:39:59 +0200

    lol

---
+++ htdocs/wiki/config.php
@@ -0,0 +1,13 @@
+<?php /* Configuration file for LionWiki. */
+$WIKI_TITLE = 'Il faut réformer le droit d\'auteur'; // name of the site
+
+// SHA1 hash of password. If empty (or commented out), no password is required
+// $PASSWORD = sha1("my_password");
+
+$TEMPLATE = 'templates/dandelion.html'; // presentation template
+
+// if true, you need to fill password for reading pages too
+// before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
+$PROTECTED_READ = false;
+
+$NO_HTML = true; // XSS protection
