085eab21239c98e4f71f241ec9fafdb9f4bc8c7d
Sploinga <github@sploing.fr>
2013-06-25 17:23:43 +0200

    wiki

--- htdocs/wiki/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("a[href^='http:']:not([href*='" + window.location.host + "'])").each(function() {
-	        $(this).attr("target", "_blank");
-    });
-	$("a[href^='https:']:not([href*='" + window.location.host + "'])").each(function() {
-	        $(this).attr("target", "_blank");
-    });
-});
