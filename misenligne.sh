242a9518cae12a0878300e57d1a3b7739c03ca06
Sploinga <github@sploing.fr>
2013-07-09 23:30:19 +0200

    lol

--- misenligne.sh
+++ misenligne.sh
@@ -6,6 +6,6 @@ cp -a  favicon.ico googlea877ebc45daa60ac.html .htaccess wiki htdocs/
 git add htdocs
 git add content
 git commit -a
-git push gandi principal
+git push gandi principal:master
 ssh 120749@git.dc0.gpaas.net 'deploy rda.sploing.fr.git'
 git push origine principal
